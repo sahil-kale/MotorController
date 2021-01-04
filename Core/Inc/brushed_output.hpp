855ef2dbd4b412495ae3e4fd0acc94eddc0613bf
dakaleyt <32375512+dakaleyt@users.noreply.github.com>
2021-01-03 22:47:45 -0700

    Finish Brushed Motor Output

---
+++ Core/Inc/brushed_output.hpp
@@ -0,0 +1,13 @@
+class BrushedMotorDriver //Singleton behaviour used to prevent 2 files from using the set functions
+{
+	public:
+		BrushedMotorDriver(const BrushedMotorDriver*) = delete; //Ensures that you don't accidently make a copy
+		static BrushedMotorDriver* GetInstance();
+
+		/* @param speed -1.0 to 1.0*/
+		bool set(double speed);
+
+	private:
+		BrushedMotorDriver(); //Private constructor
+		static BrushedMotorDriver* s_instance;
+};
